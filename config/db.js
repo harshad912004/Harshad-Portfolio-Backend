1a7f31bbf2e09d86ac429d0775fd2bca5ba1ae6f
harshad912004 <harshaddhongade0901@gmail.com>
2026-07-19 22:56:52 +0530

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,9 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+
 // Create the connection pool
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(urlDB);
 
 module.exports = pool;
